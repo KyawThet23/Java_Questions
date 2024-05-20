dee701ae4c3bf6d6ada2a4173974b1d42b4d67be
kyawThetHtun <kyawthe235@gmail.com>
2024-05-20 23:23:36 +0630

    Swap number without third variable

---
+++ SwapNumber.java
@@ -0,0 +1,17 @@
+public class SwapNumber {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a is "+a+" and b is "+b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a is "+a+" and b is "+b);
+    }
+
+
+}
