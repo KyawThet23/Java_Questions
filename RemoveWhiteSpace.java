56aeb4ab06877fdc9daab85e681ea9e309d04896
kyawThetHtun <kyawthe235@gmail.com>
2024-05-21 00:34:56 +0630

    remove leading ' '

--- RemoveWhiteSpace.java
+++ RemoveWhiteSpace.java
@@ -2,9 +2,15 @@ public class RemoveWhiteSpace {
 
     public static void main(String[] args) {
         System.out.println(removeWhiteSpace("Hell opp a"));
+
+        System.out.println(removeLeadingWhiteSpace("  abc  def\t"));
     }
 
     public static String removeWhiteSpace(String input){
         return input.replace(" ","");
+    }
+
+    public static String removeLeadingWhiteSpace(String input){
+        return input.strip();
     }
 }
