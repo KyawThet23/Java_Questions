650a4115092eb6a77bd989c3a6bb36bb61795ce9
kyawThetHtun <kyawthe235@gmail.com>
2024-05-23 22:22:11 +0630

    binary search implements

---
+++ BinarySearch.java
@@ -0,0 +1,29 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] array = {1,20,43};
+        int keyIndex = binarySearch(array,20);
+    }
+
+    public static int binarySearch(int[] array,int key){
+
+        int start = 0;
+        int end = array.length -1;
+        int mid =  (start+end)/2;
+
+        while (start <= end){
+
+            if(array[mid] == key){
+                return mid;
+            } else if (array[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+
+            mid =  (start+end)/2;
+        }
+
+        return 1;
+    }
+}
